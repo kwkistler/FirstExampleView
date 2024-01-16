b13e608221760e42d3507f4b13c5646246e9f273
Kraig Kistler <kraigwkistler@gmail.com>
2024-01-16 15:32:34 -0800

    Initial Commit

---
+++ FirstExampleView/FirstExampleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstExampleViewApp.swift
+//  FirstExampleView
+//
+//  Created by Kraig Kistler on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstExampleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
